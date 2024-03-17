b8c06af6a138dc54b22b6162185fe9022488b832
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2024-03-17 09:07:01 -0300

    first commit

---
+++ src/test/java/unrn/export/ExportTest.java
@@ -0,0 +1,14 @@
+package unrn.export;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class ExportTest {
+    @Test
+    public void test01() throws IOException {
+        var e = new Export();
+        e.export();
+        //¿ assert ?
+    }
+}
