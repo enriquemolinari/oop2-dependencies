e47a34d590551e2e950adcbf07a0d9c136f45081
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2025-03-17 16:42:50 -0300

    una solucion algo mas simple

--- src/main/java/unrn/export/Users.java
+++ src/main/java/unrn/export/Users.java
@@ -1,18 +1,32 @@
 package unrn.export;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
-import static unrn.export.User.userAs;
-
 public class Users {
-    public List<User> all() {
-        return List.of(userAs("user1", "pwd1", "bla@email.com"),
-                userAs("user2", "pwd2", "bla2@email.com"),
-                userAs("user3", "pwd3", "bla3@email.com"),
-                userAs("user4", "pwd4", "bla4@email.com"),
-                userAs("user5", "pwd5", "bla5@email.com"),
-                userAs("user6", "pwd6", "bla6@email.com"),
-                userAs("user7", "pwd7", "bla7@email.com")
-        );
+    public List<User> users;
+
+    public Users(List<User> users) {
+        this.users = users;
+    }
+
+    public void export() {
+        File aFile = new File("/home/enrique/export-users.txt");
+        try(FileWriter writer = new FileWriter(aFile)){
+            writer.write(this.toCSV());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String toCSV() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("username, email").append(System.lineSeparator());
+        for (User user : users) {
+            sb.append(user.userName()).append(",").append(user.email()).append(System.lineSeparator());
+        }
+        return sb.toString();
     }
 }
--- src/test/java/unrn/export/ExportTest.java
+++ src/test/java/unrn/export/ExportTest.java
@@ -3,12 +3,24 @@ package unrn.export;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ExportTest {
     @Test
-    public void test01() throws IOException {
-        var e = new Export();
-        e.export();
+    public void test01() {
+        var users = new Users(listaDeUsuarios());
+        users.export();
         //¿ assert ?
+    }
+
+    private static List<User> listaDeUsuarios() {
+        return List.of(new User("user1", "pwd1", "bla@email.com"),
+                new User("user2", "pwd2", "bla2@email.com"),
+                new User("user3", "pwd3", "bla3@email.com"),
+                new User("user4", "pwd4", "bla4@email.com"),
+                new User("user5", "pwd5", "bla5@email.com"),
+                new User("user6", "pwd6", "bla6@email.com"),
+                new User("user7", "pwd7", "bla7@email.com")
+        );
     }
 }
